806700f6ab89fa442c142b21b0717a21e31709e9
tsoyvi <tsoyvi@inbox.ru>
2021-11-25 18:05:46 +0500

    Lesson_5

---
+++ lesson5/tsoyvi/src/Controller/MainPageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainPageController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_page")
+     */
+    public function index(): Response
+    {
+        return $this->render('main_page/index.html.twig', [
+            'controller_name' => 'MainPageController',
+        ]);
+    }
+}
